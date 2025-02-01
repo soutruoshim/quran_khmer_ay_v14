6274398f7b7b5e9e9ffdeb0cc1dc4d28475e6af2
srhdp <srhdp@MacBook-Pro.local>
2025-02-01 09:36:37 +0700

    Datasource and Repository

--- app/src/main/java/com/sout_rahim/quran_ay/data/repository/QuranRepositoryImpl.kt
+++ app/src/main/java/com/sout_rahim/quran_ay/data/repository/QuranRepositoryImpl.kt
@@ -3,43 +3,48 @@ package com.sout_rahim.quran_ay.data.repository
 import com.sout_rahim.quran_ay.data.model.FavoriteItem
 import com.sout_rahim.quran_ay.data.model.SurahContentItem
 import com.sout_rahim.quran_ay.data.model.SurahItem
+import com.sout_rahim.quran_ay.data.repository.data_source.QuranLocalDataSource
 import com.sout_rahim.quran_ay.domain.repository.QuranRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.filterNotNull
 
-class QuranRepositoryImpl: QuranRepository {
+class QuranRepositoryImpl(
+    private val localDataSource: QuranLocalDataSource
+): QuranRepository {
     override fun getAllSurahs(): Flow<List<SurahItem>> {
-        TODO("Not yet implemented")
+        return localDataSource.getAllSurahs()
     }
 
     override fun searchSurahs(query: String): Flow<List<SurahItem>> {
-        TODO("Not yet implemented")
+        return localDataSource.searchSurahs(query)
     }
 
     override fun getSurahContent(surahId: Int): Flow<List<SurahContentItem>> {
-        TODO("Not yet implemented")
+        return localDataSource.getSurahContent(surahId)
     }
 
     override fun searchContent(query: String): Flow<List<SurahContentItem>> {
-        TODO("Not yet implemented")
+        return localDataSource.searchContent(query)
     }
 
     override fun getAyahByIndex(surahId: Int, verseId: Int): Flow<SurahContentItem> {
-        TODO("Not yet implemented")
+        return localDataSource.getAyahByIndex(surahId, verseId)
+            .filterNotNull() // Ensures the Flow does not emit null values
     }
 
     override suspend fun addBookmark(favoriteItem: FavoriteItem) {
-        TODO("Not yet implemented")
+        localDataSource.addBookmark(favoriteItem)
     }
 
     override suspend fun removeBookmark(favoriteItem: FavoriteItem) {
-        TODO("Not yet implemented")
+        localDataSource.removeBookmark(favoriteItem)
     }
 
     override suspend fun removeAllBookmarks() {
-        TODO("Not yet implemented")
+        localDataSource.removeAllBookmarks()
     }
 
     override fun getAllBookmarks(): Flow<List<FavoriteItem>> {
-        TODO("Not yet implemented")
+        return localDataSource.getAllBookmarks()
     }
 }
